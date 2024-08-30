1787f5c6f35f0274cfef36c788ee4a0698523c44
Andrii Dudar <andrij.dudar@gmail.com>
2024-08-30 22:06:38 +0300

    Solution

--- src/utils/ProtectedRouter/ProtectedRouter.jsx
+++ src/utils/ProtectedRouter/ProtectedRouter.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import { Outlet, Navigate } from "react-router-dom";
-// import useStoreAuth from "../StoreAuth";
+import useStoreAuth from "../StoreAuth";
+import { useEffect } from "react";
 // import { useEffect } from "react";
-// import Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 
-// import { SERVER_URL } from '../../services/httpClient';
+import { SERVER_URL } from '../../services/httpClient';
 
 
 export const ProtectedRoutes = () => {
-  // const accsessToken = useStoreAuth((state) => state.access_token);
-  // const setAccessToken = useStoreAuth((state) => state.setAccessToken);
-  // const refreshToken = useStoreAuth((state) => state.refresh_token);
-  // const setRefreshToken = useStoreAuth((state) => state.setRefreshToken);
+  const accsessToken = useStoreAuth((state) => state.access_token);
+  const setAccessToken = useStoreAuth((state) => state.setAccessToken);
+  const refreshToken = useStoreAuth((state) => state.refresh_token);
+  const setRefreshToken = useStoreAuth((state) => state.setRefreshToken);
   // const location = useLocation();
   // const navigate = useNavigate();
   //
-  // useEffect(() => {
+  useEffect(() => {
 
-    // const allCookies = Cookies.get();
-    // console.log('cookies:', allCookies);
+    const allCookies = Cookies.get();
+    console.log('cookies:', allCookies);
 
     // for (let i = 0; i < localStorage.length; i++) {
     //   const key = localStorage.key(i);
@@ -41,63 +42,62 @@ export const ProtectedRoutes = () => {
     // console.log('refresh_token_local:', localStorage.getItem('refresh_token'));
 
 
-    // const fetchTokens = async () => {
+    const fetchTokens = async () => {
       // Отримуємо код із URL
       // const queryParams = new URLSearchParams(location.search);
       // const code = queryParams.get('code');
 
       // if (code) {
-    //   try {
-    //     // Відправляємо код на бекенд для обміну на токен
-    //     const response = await fetch(SERVER_URL + '/auth/token', {
-    //       method: 'GET',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       credentials: 'include', // Якщо потрібно передавати куки
-    //       // params: { code },
-    //     });
+        try {
+      //     // Відправляємо код на бекенд для обміну на токен
+      const response = await fetch(SERVER_URL + "/api/auth/google_login", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include', // Якщо потрібно передавати куки
+        // params: { code },
+      });
 
-    //     if (!response.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-    //     const accessToken = response.headers.get('access_token');
-    //     const refreshToken = response.headers.get('refresh-token');
-    //     const tokenType = response.headers.get('token_type');
-    //     const allHeaders = response.headers;
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const accessToken = response.headers.get('access_token');
+      const refreshToken = response.headers.get('refresh-token');
+      const tokenType = response.headers.get('token_type');
+      const allHeaders = response.headers;
 
-    //     console.log('access_token_header:', accessToken);
-    //     console.log('refresh_token_header:', refreshToken);
-    //     console.log('token_type_header:', tokenType);
-    //     console.log('allHeaders:', allHeaders);
+      console.log('access_token_header:', accessToken);
+      console.log('refresh_token_header:', refreshToken);
+      console.log('token_type_header:', tokenType);
+      console.log('allHeaders:', allHeaders);
 
-    //     const allCookies = Cookies.get();
-    //     console.log('cookies:', allCookies);
-    //     // Отримуємо дані з відповіді
-    //     const data = await response.json();
-    //     const { access_token, refresh_token } = data;
+      const allCookies = Cookies.get();
+      console.log('cookies:', allCookies);
+      // Отримуємо дані з відповіді
+      const data = await response.json();
+      const { access_token, refresh_token } = data;
 
-    //     // Зберігаємо токен у локальному сховищі або cookies
-    //     localStorage.setItem('access_token', access_token);
-    //     localStorage.setItem('refresh_token', refresh_token);
-
-
-    //     // Зберігаємо токен у сторі
-    //     setAccessToken(access_token);
-    //     setRefreshToken(refresh_token);
+      // Зберігаємо токен у локальному сховищі або cookies
+      localStorage.setItem('access_token', access_token);
+      localStorage.setItem('refresh_token', refresh_token);
 
 
+      // Зберігаємо токен у сторі
+      setAccessToken(access_token);
+      setRefreshToken(refresh_token);
 
-    //     // Перенаправляємо користувача на головну сторінку або dashboard
-    //     // navigate('/dashboard');
-    //   } catch (error) {
-    //     console.error('Authentication failed:', error);
-    //   }
-    //   // }
-    // };
 
-    // fetchTokens();
-  // }, []);
 
-  return (true) ? <Outlet /> : <Navigate to="/login" />
+      // Перенаправляємо користувача на головну сторінку або dashboard
+      // navigate('/dashboard');
+    } catch(error) {
+    console.error('Помилка авторизації:', error);
+  }
+  };
+
+  fetchTokens();
+}, []);
+
+return (accsessToken || refreshToken) ? <Outlet /> : <Navigate to="/login" />
 };
