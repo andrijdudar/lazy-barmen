52fd6d7f3870174154e1ae9813a67db83d096899
Andrii Dudar <andrij.dudar@gmail.com>
2024-09-08 03:01:57 +0300

    Solution

--- src/components/utils/LoginEvgen/LoginEvgen.jsx
+++ src/components/utils/LoginEvgen/LoginEvgen.jsx
@@ -317,7 +317,7 @@ export const LoginEvgen = () => {
 
   return (
     <div className='login_evgen'>
-      <div id="buttonDiv"></div>
+      <div className='button btt' id="buttonDiv"></div>
       <p>Encoded JWT ID token: {token|| <span>'Не знайдено токен'</span>}</p>
     </div>
   );
